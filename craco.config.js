dc8f429ede9ccf4abf6a41a092dfc64adb5e9797
Vitalii <svitalii138@gmail.com>
2023-04-15 22:11:01 +0300

    eslint added

--- craco.config.js
+++ craco.config.js
@@ -3,13 +3,23 @@ module.exports = {
     configure: {
       resolve: {
         fallback: {
-          path: require.resolve("path-browserify"),
-          crypto: require.resolve("crypto-browserify"),
-          stream: require.resolve("stream-browserify"),
-          util: require.resolve("util/"),
-          buffer: require.resolve("buffer/"),
+          path: require.resolve('path-browserify'),
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+          util: require.resolve('util/'),
+          buffer: require.resolve('buffer/'),
         },
       },
+    },
+  },
+  eslint: {
+    enable: true /* (default value) */,
+    mode: 'extends' /* (default value) */ || 'file',
+    configure: {
+      /* ... */
+    },
+    pluginOptions: {
+      /* ... */
     },
   },
 };
